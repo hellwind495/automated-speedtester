c245d6542f72eab3fc1a03d8cff280a658ed30f2
hellwind495 <hellwind495@gmail.com>
2020-08-14 11:23:31 +0200

    Check file and more dialogue messages added

--- run-speedtester.ps1
+++ run-speedtester.ps1
@@ -2,16 +2,24 @@
 {
 Write-Output "Starting a run"
 $results = @()
-$exe = "D:\Projects\speed tests\ookla-speedtest-1.0.0-win64\speedtest.exe"
+$exe = "D:\Projects\automated-speedtester\speedtest.exe"
 $serverids = @(1746, 2789, 7982) 
 foreach($serverid in $serverids){
     Write-Output "Doing run for serverid: $serverid"
     $result = &$exe -s $serverid -f csv
     $results += $result
 }
-foreach($result in $result){
+Write-Output "Done with runs"
+$path = "D:\Projects\automated-speedtester\speedtest_results.csv"
+if(![System.IO.File]::Exists($path)){
+    '"date","server name","server id","latency","jitter","packet loss","download","upload","download bytes","upload bytes","share url"' > $path
+}
+Write-Output "Writing results"
+foreach($result in $results){
     $date = Get-Date
-    "`"$date`",$result" >> "D:\Projects\speed tests\ookla-speedtest-1.0.0-win64\speedtest_results.csv"
+    "`"$date`",$result" >> "$path"
     }
+$waittime = 600
+Write-Output "Done writing results. Waiting for $waittime seconds.\n"
 Start-Sleep -s 600
 }
