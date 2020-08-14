c5befdb1a169dde86bf4ef5e28b89e27fbc2045e
hellwind495 <hellwind495@gmail.com>
2020-08-14 11:14:04 +0200

    added initial files

---
+++ run-speedtester.ps1
@@ -0,0 +1,17 @@
+﻿while ($true)
+{
+Write-Output "Starting a run"
+$results = @()
+$exe = "D:\Projects\speed tests\ookla-speedtest-1.0.0-win64\speedtest.exe"
+$serverids = @(1746, 2789, 7982) 
+foreach($serverid in $serverids){
+    Write-Output "Doing run for serverid: $serverid"
+    $result = &$exe -s $serverid -f csv
+    $results += $result
+}
+foreach($result in $result){
+    $date = Get-Date
+    "`"$date`",$result" >> "D:\Projects\speed tests\ookla-speedtest-1.0.0-win64\speedtest_results.csv"
+    }
+Start-Sleep -s 600
+}
